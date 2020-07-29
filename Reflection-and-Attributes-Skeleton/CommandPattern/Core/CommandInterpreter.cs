47071b16958ddac7848860615271330b29c1503a
Zhivko Micikulev <63278347+ZhivkoMicikulev@users.noreply.github.com>
2020-07-29 16:32:07 +0300

    Reflection and Attributes

---
+++ Reflection-and-Attributes-Skeleton/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,50 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string COMMAND_POSTFIX = "Command";
+        
+        public CommandInterpreter()
+        {
+
+        }
+        
+        public string Read(string args)
+        {
+            string[] commandTokens = args
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .ToArray();
+
+            var commandName = commandTokens[0]+COMMAND_POSTFIX;
+            var commandArgs = commandTokens.Skip(1)
+                .ToArray();
+           
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+            Type commandType = assembly.GetTypes()
+                .FirstOrDefault(
+                t => t.Name.ToLower()==commandName.ToLower()
+                );
+
+            if (commandType==null)
+            {
+                throw new ArgumentException("Invalid command type !");
+            }
+
+
+            ICommand commandInstance = (ICommand)Activator.CreateInstance(commandType);
+
+
+                var result = commandInstance.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ Reflection-and-Attributes-Skeleton/CommandPattern/Core/Models/Comands/ByeCommand.cs
@@ -0,0 +1,15 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Core.Models.Comands
+{
+    public class ByeCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            return $"Bye , {args[0]}";
+        }
+    }
+}
